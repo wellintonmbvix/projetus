1a734c089b1cd66335ab0816697da447fc1ece93
wellintonmbvix <wellinton.m.b.vix@gmail.com>
2025-04-16 17:13:18 -0300

    Definindo host Swagger

--- projetus_api/src/view/api.swagger.doc.pas
+++ projetus_api/src/view/api.swagger.doc.pas
@@ -12,14 +12,16 @@ implementation
 procedure StartDocumentation;
 begin
   Swagger
-    .Info
-      .Title('API Projetus')
-      .Description('API para comunicação do app')
-        .Contact
-          .Name('Wellinton Mattos Brozeghini')
-          .Email('wellinton@projetus.com.br')
-          .URL('http://www.projetus.com.br')
-        .&End
+    .Host('127.0.0.1')
+      .Info
+        .Title('API Projetus')
+        .Description('API para comunicação do app')
+          .Contact
+            .Name('Wellinton Mattos Brozeghini')
+            .Email('wellinton@projetus.com.br')
+            .URL('http://www.projetus.com.br')
+          .&End
+      .&End
     .&End
   .&End;
 end;
