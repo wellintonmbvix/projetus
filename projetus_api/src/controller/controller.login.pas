2eb20b7f4513f59c05f032cfbd1e233dee66b305
wellintonmbvix <wellinton.m.b.vix@gmail.com>
2025-04-30 18:14:06 -0300

    Implementação de Token JWT

--- projetus_api/src/controller/controller.login.pas
+++ projetus_api/src/controller/controller.login.pas
@@ -5,12 +5,19 @@ interface
 uses
   Horse,
   Horse.Commons,
+  Horse.JWT,
+
+  JOSE.Core.JWT,
+  JOSE.Core.Builder,
+
   System.Classes,
   System.SysUtils,
   System.Generics.Collections,
   System.JSON,
   System.StrUtils,
   System.Math,
+  System.DateUtils,
+
   Data.DB,
   DBClient,
 
@@ -18,7 +25,7 @@ uses
   BCrypt.Types,
 
   Horse.GBSwagger.Register,
-  Horse.GBSwagger.Controller,
+  Horse.GBSwagger.controller,
   GBSwagger.Path.Attributes,
 
   model.api.message,
@@ -30,6 +37,9 @@ uses
 type
   [SwagPath('v1', 'Login')]
   TControllerLogin = class(THorseGBSwagger)
+  private
+    function CriarToken(usuario: Tusuarios): String;
+  public
     class procedure Registry;
 
     [SwagPost('login', 'Realiza login na API')]
@@ -40,16 +50,35 @@ type
     class procedure Post(Req: THorseRequest; Res: THorseResponse; Next: TProc);
   end;
 
+const JWTPassword = 'suaSenhaMaisFraca';
+
 implementation
 
 { TControllerLogin }
+
+function TControllerLogin.CriarToken(usuario: Tusuarios): String;
+var
+  LJWT: TJWT;
+  LToken: String;
+begin
+  LJWT := TJWT.Create();
+  Try
+    LJWT.Claims.Expiration := IncHour(Now, 1);
+    LJWT.Claims.JWTId := usuario.id_usuario.ToString();
+    LToken := TJOSE.SHA256CompactToken(JWTPassword, LJWT);
+  Finally
+    FreeAndNil(LJWT);
+  End;
+
+  Result := LToken;
+end;
 
 class procedure TControllerLogin.Post(Req: THorseRequest; Res: THorseResponse;
   Next: TProc);
 var
   oJson: TJSONObject;
   passwordRehashNeeded: Boolean;
-  Usuarios: TObjectList<Tusuarios>;
+  usuarios: TObjectList<Tusuarios>;
   filter: String;
 begin
   if Req.Body.IsEmpty then
@@ -57,53 +86,52 @@ begin
 
   oJson := Req.Body<TJSONObject>;
   if (oJson.GetValue<String>('nome_usuario').IsEmpty) Or
-  (oJson.GetValue<String>('senha_usuario').IsEmpty) then
-    begin
-      oJson.SetPairs(TList<TJSONPair>.Create);
-      oJson.AddPair('message', 'O nome e senha do usuário são campos obrigatório!');
-      Res.Send<TJSONObject>(oJson).Status(401);
-      Exit;
-    end;
+    (oJson.GetValue<String>('senha_usuario').IsEmpty) then
+  begin
+    oJson.SetPairs(TList<TJSONPair>.Create);
+    oJson.AddPair('message',
+      'O nome e senha do usuário são campos obrigatório!');
+    Res.Send<TJSONObject>(oJson).Status(401);
+    Exit;
+  end;
 
-  filter := 'dt_del is null AND nome_usuario='+
+  filter := 'dt_del is null AND nome_usuario=' +
     QuotedStr(oJson.GetValue<String>('nome_usuario'));
   var
   IUsuario := TIUsuario.New;
-  IUsuario.Build.ListAll(filter, Usuarios, '');
+  IUsuario.Build.ListAll(filter, usuarios, '');
 
-  if Usuarios.Count = 0 then
+  if usuarios.Count = 0 then
+  begin
+    oJson.SetPairs(TList<TJSONPair>.Create);
+    oJson.AddPair('message', 'Invalid username or password');
+    Res.Send<TJSONObject>(oJson).Status(401);
+  end
+  else
+  begin
+    var
+    isPasswordValid := TBCrypt.CompareHash
+      (oJson.GetValue<String>('senha_usuario'), usuarios.Last.senha_hash);
+
+    if not isPasswordValid then // Se senha é ou não válida
     begin
       oJson.SetPairs(TList<TJSONPair>.Create);
-      oJson.AddPair('message', 'Invalid username or password');
+      oJson.AddPair('mesage', 'invalid username or password');
       Res.Send<TJSONObject>(oJson).Status(401);
     end
-  else
+    else
     begin
-      var isPasswordValid := TBCrypt.CompareHash(
-      oJson.GetValue<String>('senha_usuario'), Usuarios.Last.senha_hash);
-
-      if not isPasswordValid then // Se senha é ou não válida
-        begin
-          oJson.SetPairs(TList<TJSONPair>.Create);
-          oJson.AddPair('mesage', 'invalid username or password');
-          Res.Send<TJSONObject>(oJson).Status(401);
-        end
-      else
-        begin
-          oJson.SetPairs(TList<TJSONPair>.Create);
-          oJson.AddPair('mesage', 'user is valid');
-          Res.Send<TJSONObject>(oJson).Status(200);
-        end;
-
+      oJson.SetPairs(TList<TJSONPair>.Create);
+      oJson.AddPair('mesage', 'user is valid');
+      Res.Send<TJSONObject>(oJson).Status(200);
     end;
+
+  end;
 end;
 
 class procedure TControllerLogin.Registry;
 begin
-  THorse
-    .Group
-      .Prefix('api/v1')
-        .Post('/login', Post);
+  THorse.Group.Prefix('api/v1').Post('/login', Post);
 end;
 
 initialization
